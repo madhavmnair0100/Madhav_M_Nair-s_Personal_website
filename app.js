f8f5f8404ce10d55f9908bc46114ac4c734726a3
Madhav M Nair <121674886+madhavmnair0100@users.noreply.github.com>
2023-05-08 17:12:18 +0530

    Updated app.js for Starting Buffer

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var spinnerRouter = require('./routes/router')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -19,7 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', spinnerRouter);
+app.use('/home', indexRouter)
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
